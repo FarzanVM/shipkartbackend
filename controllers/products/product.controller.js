487229d4bc61698a2abbf7b007fed803d3b90743
FarzanVM <farzanmohan@gmail.com>
2024-05-02 23:09:11 +0530

    changed response message in updateproduct

--- controllers/products/product.controller.js
+++ controllers/products/product.controller.js
@@ -59,7 +59,7 @@ const updateProduct = async(req,res) =>{
             return res.status(500).json({message:"Product doesnt exist"})
         }
         const updatedProduct = await Product.updateOne({_id:req.body._id}, {$set : req.body})
-        res.status(200).json(updatedProduct)
+        res.status(200).json({message:"Product Updated"})
     }
     catch(error){
         res.status(500).json({message:error.message})
